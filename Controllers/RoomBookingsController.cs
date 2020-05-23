3f182d8438a9c45225a1c4173b45e8a14d7879e1
Fatima Gušić <45359339+FatimaGusic@users.noreply.github.com>
2020-05-23 20:25:53 +0200

    Add files via upload

---
+++ Controllers/RoomBookingsController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using HSM.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace HSM.Controllers
+{
+    public class RoomBookingsController : Controller
+    {
+       
+        private HotelEntities db = new HotelEntities();
+
+     
+
+
+        // GET: RoomBookings
+        public ActionResult Index()
+        {
+            return View(db.RoomBookings.ToList());
+        }
+
+        // GET: RoomBookings/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RoomBooking roomBooking = db.RoomBookings.Find(id);
+            if (roomBooking == null)
+            {
+                return HttpNotFound();
+            }
+            return View(roomBooking);
+        }
+
+        // GET: RoomBookings/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RoomBookings/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "BookingId,CustomerName,CustomerAddress,BookingFrom,BookingTo,AssignRoomId,NoOfMembers,TotalAmount")] RoomBooking roomBooking)
+        {
+            if (ModelState.IsValid)
+            {
+                db.RoomBookings.Add(roomBooking);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(roomBooking);
+        }
+
+        // GET: RoomBookings/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RoomBooking roomBooking = db.RoomBookings.Find(id);
+            if (roomBooking == null)
+            {
+                return HttpNotFound();
+            }
+            return View(roomBooking);
+        }
+
+        // POST: RoomBookings/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "BookingId,CustomerName,CustomerAddress,BookingFrom,BookingTo,AssignRoomId,NoOfMembers,TotalAmount")] RoomBooking roomBooking)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(roomBooking).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(roomBooking);
+        }
+
+        // GET: RoomBookings/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RoomBooking roomBooking = db.RoomBookings.Find(id);
+            if (roomBooking == null)
+            {
+                return HttpNotFound();
+            }
+            return View(roomBooking);
+        }
+
+        // POST: RoomBookings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            RoomBooking roomBooking = db.RoomBookings.Find(id);
+            db.RoomBookings.Remove(roomBooking);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Controllers/RoomController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HSM.Models;
+using HSM.ViewModel;
+
+namespace HSM.Controllers
+{
+   
+    public class RoomController : Controller
+    {
+        private HotelEntities objHotelEntities;
+
+        public RoomController()
+        {
+            objHotelEntities = new HotelEntities();
+        }
+        // GET: Room
+        [Authorize(Roles = "Admin")]
+        public ActionResult Index()
+        {
+            RoomViewModel objRoomViewModel = new RoomViewModel();
+            objRoomViewModel.ListOfBookingStatus = (from obj in objHotelEntities.BookingStatus
+                                                    select new SelectListItem()
+                                                    {
+                                                        Text = obj.BookingStatus,
+                                                        Value = obj.BookingStatusId.ToString()
+                                                    }).ToList();
+
+            objRoomViewModel.ListOfRoomType = (from obj in objHotelEntities.RoomTypes
+                                               select new SelectListItem()
+                                               {
+                                                   Text = obj.RoomType1,
+                                                   Value = obj.RoomTypeId.ToString()
+                                               }).ToList();
+
+            return View(objRoomViewModel);
+        }
+        [HttpPost]
+        public ActionResult Index(RoomViewModel objRoomViewModel)
+        {
+            string message = String.Empty;
+            string ImageUniqueName = String.Empty;
+            string ActualImageName = String.Empty;
+            if (objRoomViewModel.RoomId == 0)
+            {
+                ImageUniqueName = Guid.NewGuid().ToString();
+                ActualImageName = ImageUniqueName + Path.GetExtension(objRoomViewModel.Image.FileName);
+
+                objRoomViewModel.Image.SaveAs(Server.MapPath("~/RoomImages/" + ActualImageName));
+                //objHotelDbEntities
+                Room objRoom = new Room()
+                {
+                    RoomNumber = objRoomViewModel.RoomNumber,
+                    RoomDescription = objRoomViewModel.RoomDescription,
+                    RoomPrice = objRoomViewModel.RoomPrice,
+                    BookingStatusId = objRoomViewModel.BookingStatusId,
+                    IsActive = true,
+                    RoomImage = ActualImageName,
+                    RoomCapacity = objRoomViewModel.RoomCapacity,
+                    RoomTypeId = objRoomViewModel.RoomTypeId,
+                    RoomId = objRoomViewModel.RoomId
+
+
+                };
+                objHotelEntities.Rooms.Add(objRoom);
+                message = "Added.";
+            }
+            else
+            {
+                Room objRoom = objHotelEntities.Rooms.Single(model => model.RoomId == objRoomViewModel.RoomId);
+                if (objRoomViewModel.Image != null)
+                {
+                    ImageUniqueName = Guid.NewGuid().ToString();
+                    ActualImageName = ImageUniqueName + Path.GetExtension(objRoomViewModel.Image.FileName);
+                    objRoomViewModel.Image.SaveAs(Server.MapPath("~/RoomImages/" + ActualImageName));
+                    objRoom.RoomImage = ActualImageName;
+                }
+
+
+                objRoom.RoomNumber = objRoomViewModel.RoomNumber;
+                objRoom.RoomDescription = objRoomViewModel.RoomDescription;
+                objRoom.RoomPrice = objRoomViewModel.RoomPrice;
+                objRoom.BookingStatusId = objRoomViewModel.BookingStatusId;
+                objRoom.IsActive = true;
+                objRoom.RoomCapacity = objRoomViewModel.RoomCapacity;
+                objRoom.RoomTypeId = objRoomViewModel.RoomTypeId;
+                objRoom.RoomId = objRoomViewModel.RoomId;
+
+                message = "Updated.";
+            }
+
+
+            objHotelEntities.SaveChanges();
+            return Json(data: new { message = "Room Successfully" + message, success = true }, JsonRequestBehavior.AllowGet);
+
+        }
+
+        public PartialViewResult GetAllRooms()
+        {
+            IEnumerable<RoomDetailsViewModel> listOfRoomDetailsViewModels =
+                (from objRoom in objHotelEntities.Rooms
+                 join objBooking in objHotelEntities.BookingStatus on objRoom.BookingStatusId equals objBooking.BookingStatusId
+                 join objRoomType in objHotelEntities.RoomTypes on objRoom.RoomTypeId equals objRoomType.RoomTypeId
+                 select new RoomDetailsViewModel()
+                 {
+                     RoomNumber = objRoom.RoomNumber,
+                     RoomDescription = objRoom.RoomDescription,
+                     RoomCapacity = objRoom.RoomCapacity,
+                     RoomPrice = objRoom.RoomPrice,
+                     BookingStatus = objBooking.BookingStatus,
+                     RoomType = objRoomType.RoomType1,
+                     RoomImage = objRoom.RoomImage,
+                     RoomId = objRoom.RoomId
+
+                 }).ToList();
+            return PartialView("_RoomDetailsPartial", listOfRoomDetailsViewModels);
+        }
+
+        [HttpGet]
+        public JsonResult EditRoomDetails(int roomId)
+        {
+            var result = objHotelEntities.Rooms.Single(model => model.RoomId == roomId);
+            return Json(data: "", JsonRequestBehavior.AllowGet);
+        }
+    }
+}
