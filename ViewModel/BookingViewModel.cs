b16b2da07d122b92fd0228127a12d1c1ae02fd09
Lejla Tašaković <51216695+LejlaTasakovic@users.noreply.github.com>
2020-05-23 22:55:55 +0200

    Add files via upload

---
+++ ViewModel/BookingViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace HSM.ViewModel
+{
+    public class BookingViewModel
+    {
+        public int BookingId { get; set; }
+
+        [Display(Name = "Customer Name")]
+        [Required(ErrorMessage = "Customer name is required.")]
+        public string CustomerName { get; set; }
+
+        [Display(Name = "Customer Address")]
+        [Required(ErrorMessage = "Customer address is required.")]
+        public string CustomerAddress { get; set; }
+
+        [Display(Name = "Booking From")]
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Booking from is required.")]
+
+        public DateTime BookingFrom { get; set; }
+
+        [Display(Name = "Booking To")]
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Booking to is required.")]
+
+        public DateTime BookingTo { get; set; }
+
+        [Display(Name = "Assign Room")]
+        [Required(ErrorMessage = "Room is required.")]
+        public int AssignRoomId { get; set; }
+
+        [Display(Name = "Number of members")]
+        [Required(ErrorMessage = "Number of members is required.")]
+        public int NumberOfMembers { get; set; }
+        public IEnumerable<SelectListItem> ListOfRooms { get; set; }
+
+    }
+}
