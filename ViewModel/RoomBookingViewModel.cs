99744dc2a5259b1386efa48fe09066794836de6b
Emir1414 <57076547+Emir1414@users.noreply.github.com>
2020-05-23 21:30:44 +0200

    Add files via upload

---
+++ ViewModel/RoomBookingViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HSM.ViewModel
+{
+    public class RoomBookingViewModel
+    {
+        public int BookingId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerAddress { get; set; }
+        public int NumberOfMembers { get; set; }
+        public DateTime BookingFrom { get; set; }
+        public DateTime BookingTo { get; set; }
+        public string RoomNumber { get; set; }
+
+
+    }
+}
