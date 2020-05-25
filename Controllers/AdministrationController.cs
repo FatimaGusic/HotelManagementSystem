1ebd61bc33cdca7d985d0d0e3180dd42a805f0b0
mirza998 <mirza.jasarspahic@hotmail.com>
2020-05-25 11:25:31 +0200

    Add files via upload

---
+++ Controllers/AdministrationController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HSM.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        // GET: Administration
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+    }
+}
